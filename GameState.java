2d6bc87545b2a09a17939582c75590b61a7a71f2
anshag01 <ansh.agrawal@mail.utoronto.ca>
2023-07-24 19:29:14 -0400

    nimnumber

--- GameState.java
+++ GameState.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameState {
+
+    String player;
+    ArrayList<Integer> choices;
+    int piles;
+    ArrayList<GameState> childState;
+    boolean isEndState = false;
+    int value;
+
+    public GameState(String player, int piles){
+        this.player = player;
+        this.piles = piles;
+        choices = new ArrayList<>(List.of(1, 2, 3));
+        if (piles<=0){
+            isEndState = true;
+        }
+    }
+
+    public ArrayList<GameState> generateChild() {
+        String newPlayer = player.equals("computer") ? "human" : "computer";
+        ArrayList<GameState> gameStates = new ArrayList<>();
+
+        for (int i = 0; i < choices.size(); i++) {
+            int gameStateValue = choices.get(i);
+            int gamePileNumber = this.piles - gameStateValue;
+            if(gamePileNumber<0) continue;
+            GameState newstate = new GameState(newPlayer, gamePileNumber);
+            gameStates.add(newstate);
+            newstate.value = gameStateValue;
+        }
+        this.childState = gameStates;
+        return gameStates;
+    }
 }
--- GameTree.java
+++ GameTree.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameTree {
+
+    GameState head;
+
+    public GameTree(int piles, String player){
+        this.head = new GameState(player, piles);
+    }
+
+    public void generateTree(){
+        if(this.head.isEndState){
+            return;
+        }
+        ArrayList<GameState> queue = new ArrayList<>(List.of(this.head));
+        while (queue.size()>0){
+            GameState currentState = queue.get(0);
+            queue.remove(0);
+            ArrayList<GameState> childStates = currentState.generateChild();
+            for(GameState state: childStates){
+                if(state.isEndState == false){
+                    queue.add(state);
+                }
+            }
+        }
+    }
+
+    public static void printTree(){
+        GameTree tree = new GameTree(5, "human");
+    }
+
+    public static void main(String args[]){
+        GameTree.printTree();
+    }
+
 }
